506532844b3c5f7d278e98d653806bd076d75599
evandro martins <138086339+Evandro700@users.noreply.github.com>
2024-09-17 21:05:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+    const usuarioTeste = {
+        email: 'poucas',
+    senha: '1234'
+      };
+
+    const loginButton = document.getElementById('login-button');
+    
+      loginButton.addEventListener('click', (e) => {
+        const emailInput = document.getElementById('email');
+    const senhaInput = document.getElementById('senha');
+
+    const usuario = {
+        email: emailInput.value,
+    senha: senhaInput.value
+        };
+
+    if (usuario.email === usuarioTeste.email && usuario.senha === usuarioTeste.senha) {
+        alert('Login bem-sucedido!');
+        } else {
+        alert('Email ou senha incorretos!');
+        }
+      });
